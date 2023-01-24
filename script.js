0213cd11a3c448ac3d0dc3ad98640bf9ad7680e2
meshubhampol <maheshpol9@gmail.com>
2023-01-24 11:52:00 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+
+let inputbox=document.getElementById('evaluatedText');
+let score=document.getElementById('letterCount');
+
+inputbox.addEventListener('keydown',updateScore);
+inputbox.addEventListener('keyup',updateScore);
+
+function updateScore() {
+    let length=inputbox.value.length;
+    score.innerHTML=length;
+}
